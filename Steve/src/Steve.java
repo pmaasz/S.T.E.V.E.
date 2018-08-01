820a064053112313086236776b8fca2d30733691
Philip M <Philipnormal@Philip.fritz.box>
2018-08-01 21:02:04 +0200

    everything works except blinking

--- Steve/src/Steve.java
+++ Steve/src/Steve.java
@@ -11,13 +11,7 @@ public class Steve {
         while(true)
         {
             int counter = 0;
-/*
-            //boundaries
-            if(leftEye.getLeftXPos() < 10) leftEye.setLeftXPos(10);
-            if(leftEye.getLeftXPos() > 110) leftEye.setLeftXPos(110);
-            if(rightEye.getRightXPos() < 10) rightEye.setRightXPos(110);
-            if(rightEye.getRightXPos() > 110) rightEye.setRightYPos(10);
-*/
+
             //resizing height
             if((leftEye.getLeftYPos() > 90 || leftEye.getLeftYPos() < 30) && (rightEye.getRightYPos() > 90 || rightEye.getRightYPos() < 30))
             {
@@ -35,41 +29,28 @@ public class Steve {
             if(leftEye.getLeftYPos() > 110)
             {
                 leftEye.setLeftYPos(110);
+                frame.repaintDrawPanel();
             }
 
             if(rightEye.getRightYPos() > 110)
             {
                 rightEye.setRightYPos(110);
+                frame.repaintDrawPanel();
             }
 
             //lower boundary
             if(leftEye.getLeftYPos() < 10)
             {
                 leftEye.setLeftYPos(10);
+                frame.repaintDrawPanel();
             }
 
             if(rightEye.getRightYPos() < 10)
             {
                 rightEye.setRightYPos(10);
+                frame.repaintDrawPanel();
             }
 
-/*
-            //has to be dependent of the right eye xpos
-            if(leftEye.getRightXPos() > 100)
-            {
-                rightEye.setWidthright(70);
-            } else {
-                rightEye.setWidthright(80);
-            }
-
-            //has to be dependent of the left eye xpos
-            if(leftEye.getLeftXPos() < 25)
-            {
-                leftEye.setLeftXPos(70);
-            } else {
-                leftEye.setLeftXPos(80);
-            }
-*/
             //moving up
             if(leftEye.getLeftYPos() < frame.getMouseY() && rightEye.getRightYPos() < frame.getMouseY())
             {
@@ -93,7 +74,52 @@ public class Steve {
                 rightEye.setRightYPos(rightYPos);
                 frame.repaintDrawPanel();
             }
-/*
+
+            //resizing width
+            if(leftEye.getLeftXPos() < 25)
+            {
+                leftEye.setWidthleft(70);
+                frame.repaintDrawPanel();
+            } else {
+                leftEye.setWidthleft(80);
+                frame.repaintDrawPanel();
+            }
+
+            if(rightEye.getRightXPos() > 200)
+            {
+                rightEye.setWidthright(70);
+                frame.repaintDrawPanel();
+            } else {
+                rightEye.setWidthright(80);
+                frame.repaintDrawPanel();
+            }
+
+            //left boundary
+            if(leftEye.getLeftXPos() < 10)
+            {
+                leftEye.setLeftXPos(10);
+                frame.repaintDrawPanel();
+            }
+
+            if(rightEye.getRightXPos() < 110)
+            {
+                rightEye.setRightXPos(110);
+                frame.repaintDrawPanel();
+            }
+
+            //right boundary
+            if(leftEye.getLeftXPos() > 110)
+            {
+                leftEye.setLeftXPos(110);
+                frame.repaintDrawPanel();
+            }
+
+            if(rightEye.getRightXPos() > 210)
+            {
+                rightEye.setRightXPos(210);
+                frame.repaintDrawPanel();
+            }
+
             //moving right
             if(leftEye.getLeftXPos() < frame.getMouseX() && rightEye.getRightXPos() < frame.getMouseX())
             {
@@ -117,7 +143,7 @@ public class Steve {
                 rightEye.setRightXPos(rightXPos);
                 frame.repaintDrawPanel();
             }
-*/
+
             //blinking
             repo.blink(eyes, counter);
 
